e909403919a45ca3e1913df3c8ea0e3935fe3de0
step135 <mmir@seznam.cz>
2024-01-17 14:35:55 +0100

    font-size line-height

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  publicPath: "./",
-  outputDir: "./team",
-  chainWebpack: config => {
-    config.optimization.minimizer("terser").tap(args => {
-      args[0].terserOptions.compress = { drop_console: true };
-      args[0].terserOptions.output = { comments: false, beautify: false };
-      return args;
-    });
-  }
+    publicPath: "./",
+    outputDir: "./team",
+    chainWebpack: (config) => {
+        config.optimization.minimizer("terser").tap((args) => {
+            args[0].terserOptions.compress = { drop_console: true };
+            args[0].terserOptions.output = { comments: false, beautify: false };
+            return args;
+        });
+    },
 };
